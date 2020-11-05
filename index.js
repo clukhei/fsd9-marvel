dfa4ed85f18bec4a2b96a25ad34f6e9382bc6ea8
Lu Khei <kheiboardwarrior@gmail.com>
2020-11-05 12:44:55 +0800

    display thumbnails and style with grid and bootstrap

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
-const fetch = require('node-fetch')
-const withQuery = require('with-query').default
-require('dotenv').config()
+const fetch = require("node-fetch");
+const withQuery = require("with-query").default;
+require("dotenv").config();
+const express = require("express");
+const hbs = require("express-handlebars");
+const app = express();
+const md5 = require("md5");
 
-const marvelApiUrl = 'https://gateway.marvel.com/v1/public/characters'
-const url = withQuery(marvelApiUrl, {
-    apikey:`${process.env.apikey}`,
-    hash: `${process.env.hash}`,
-    ts:  `${process.env.ts}`
-})
+app.engine("hbs", hbs({ defaultLayout: "default.hbs" }));
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + '/static'))
 
-fetch(url).then(results => results.json())
-            .then(json => console.log(json.data.results))
-        
+const PORT = parseInt(process.argv[2]) || parseInt(process.env.PORT) || 3000;
+const ts = new Date();
+const hash = md5(`${ts}${process.env.private_key}${process.env.apikey}`);
+console.log(hash);
+
+app.get("/", (req, res) => {
+	fetch(spideyUrl)
+		.then((results) => results.json())
+		.then((json) => {
+			console.log(json.data.results);
+			const requiredData = json.data.results.map((c) => {
+                const name = c.name
+                const id = c.id
+                const detailsUrl = c.urls[0].url
+                const thumbnail = c.thumbnail.path + "." + c.thumbnail.extension;
+                return {name, id, detailsUrl,thumbnail}
+			});
+		
+			if (json.data.count <= 0) {
+				return Promise.reject('Not found');
+			}
+
+			res.status(200);
+			res.type("text/html");
+			res.render("index", {
+				requiredData
+			});
+        })
+        .catch(e=> console.error('Error', e))
+});
+
+const marvelApiUrl = "https://gateway.marvel.com/v1/public/characters";
+const spideyUrl = withQuery(marvelApiUrl, {
+	apikey: `${process.env.apikey}`,
+	hash: hash,
+    ts: `${ts}`,
+    limit: 50,
+    //nameStartsWith: "dr"
+});
+
+app.use((req, res) => {
+	res.redirect("/");
+});
+app.listen(PORT, () => {
+	console.log(`Marvel PORT ${PORT} is up and running`);
+});
